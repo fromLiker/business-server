1b20d9c4f592cf52a7eb6b1f064fb26ecb9bf9d3
xuexp <xuexp@cn.ibm.com>
2020-04-19 22:10:05 +0800

    consumes = MediaType.APPLICATION_JSON_VALUE

--- src/main/java/io/seata/sample/feign/OrderApi.java
+++ src/main/java/io/seata/sample/feign/OrderApi.java
@@ -1,6 +1,7 @@
 package io.seata.sample.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.*;
 import io.seata.sample.entity.OrdersEntity;
@@ -14,8 +15,8 @@ public interface OrderApi {
     /**
      * a创建订单
      */
-    // @RequestMapping(value = "/order/create", method = RequestMethod.POST)
-    @RequestMapping("/order/create")
+    @RequestMapping(value = "/order/create", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    // @RequestMapping("/order/create")不指定默认为get
     String create(@RequestBody OrdersEntity ordersEntity);
     
 }
