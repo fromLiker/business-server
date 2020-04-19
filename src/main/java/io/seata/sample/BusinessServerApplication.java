989d64a229e0b49ae8936117e87d74c42fe392dc
xuexp <xuexp@cn.ibm.com>
2020-04-19 14:56:06 +0800

    update

--- src/main/java/io/seata/sample/BusinessServerApplication.java
+++ src/main/java/io/seata/sample/BusinessServerApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
- * aa订单服务
+ * aaa订单服务
  * @author Liker
  */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
